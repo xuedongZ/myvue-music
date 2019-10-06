31befeb40cc39721a8f135fc407b00b6b8757e8a
xuedongZ <xuedong6624@gmail.com>
2019-10-06 23:14:21 +0800

    chapter2

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 // import store from './store'
+import fastclick from 'fastclick'
 
 // Vue.config.productionTip = false
 import './common/stylus/index.styl'
+fastclick.attach(document.body)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
